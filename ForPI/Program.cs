344864db0aec7d9666ae17cee903b36b62a3e8d4
Avolk01 <63004968+Avolk01@users.noreply.github.com>
2023-01-14 15:53:54 +0300

    add some tests

---
+++ ForPI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ForPI
+{
+    public class Program
+    {
+        static void main()
+        {
+            //int n = int.Parse(Console.ReadLine());
+            var n = 10;
+            Console.WriteLine(Fact(n));
+        }
+
+        static public double Fact(int n)
+        {
+            if (n < 1)
+                return 1;
+            return n * Fact(n - 1);
+        }
+        
+    } 
+
+}
