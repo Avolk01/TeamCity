b12c05a7d059cf7e7180bff367125e232e086ec1
Avolk01 <63004968+Avolk01@users.noreply.github.com>
2023-01-14 15:10:16 +0300

    Upload C# project

---
+++ ForPI/ForPI/Program.cs
@@ -0,0 +1,12 @@
+﻿
+static double Fact(int n)
+{
+    if (n < 1)
+        return 1;
+    return n * Fact(n - 1);
+}
+
+//int n = int.Parse(Console.ReadLine());
+var n = 10;
+Console.WriteLine(Fact(n));
+
